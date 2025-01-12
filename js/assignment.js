8a988ea98436c377dc09e6e4234efe61ea3e45b1
codeL0gic <shannon.neumann@yum.com>
2025-01-12 13:09:44 -0500

    main

--- js/assignment.js
+++ js/assignment.js
@@ -3,33 +3,40 @@
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
 const yearElement = document.getElementById("year");
-const resolutionElement = document.getElementById("resolution")
-const willMeetResolutionElement = document.getElementById("will-meet-resolution")
-const submissionBtn = document.getElementById("submission-btn")
+const resolutionElement = document.getElementById("resolution");
+const willMeetResolutionElement = document.getElementById(
+  "will-meet-resolution"
+);
+const submissionBtn = document.getElementById("submission-btn");
 
 // Declare your variables here.
+const resolution = "Do more Yoga.";
+const currentYear = 2025;
+let willMeetResolution = "Yes!";
 
 function updateYear() {
-    // Update this function
-
+  // Update this function
+  yearElement.innerText = currentYear;
 }
 
 function updateResolution() {
-    // Update this function 
-
+  // Update this function
+  resolutionElement.innerText = resolution;
 }
 
 function updateWillMeetResolution() {
-    // Update this function
-
+  // Update this function
+  willMeetResolutionElement.innerText = willMeetResolution;
 }
 
 function render() {
-    // Update this function
-
+  // Update this function
+  updateYear();
+  updateResolution();
+  updateWillMeetResolution();
 }
 
 submissionBtn.addEventListener("click", function () {
-    // Update this function
-
-})
+  // Update this function
+  render();
+});
--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+const myName = "Shannon";
+console.log(myName);
+console.log(typeof myName);
 
+const four = 4;
+console.log(four);
+console.log(typeof four);
+
+let isStudent = true;
+console.log(isStudent);
+console.log(typeof isStudent);
+
+isStudent = true;
+
+console.log(isStudent);
+console.log(typeof isStudent);
+/*
